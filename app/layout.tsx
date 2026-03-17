325a7492aa97867c282c3121ba6ab2405378d066
Aditya Pramana Putra <adityapramid25@gmail.com>
2026-03-18 00:24:49 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     icon: '/skanilantech.png?v=2',
   },
   verification: {
-    google: 'vyN5yBdV4a9eJyCohsf565h3VJ0j42HSUUnwY2EmzrM',
+    google: 'google-site-verification=vyN5yBdV4a9eJyCohsf565h3VJ0j42HSUUnwY2EmzrM',
   },
 };
 
