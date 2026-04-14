1c33c898b242bae7c82fe5c4f52274c8b01d159d
Aditya Pramana Putra <adityapramid25@gmail.com>
2026-04-14 08:17:40 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://skanilantechsmg.vercel.app/sitemap.xml',
+    sitemap: 'https://skanilan.tech/sitemap.xml',
   };
 }
