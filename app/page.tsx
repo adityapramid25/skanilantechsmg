29f26e837b6421d1355fa238a7190707f28c19cc
Aditya Pramana Putra <adityapramid25@gmail.com>
2026-04-17 09:05:34 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { HeroCarousel } from '@/components/HeroCarousel';
-import { DiscountSection } from '@/components/DiscountSection';
 import { VideoAdSection } from '@/components/VideoAdSection';
 import { FunfactsSection } from '@/components/FunfactsSection';
 
@@ -7,7 +6,6 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-slate-50">
       <HeroCarousel />
-      <DiscountSection />
       <VideoAdSection />
       <FunfactsSection />
     </div>
