607bfe2031a94f2f594eb5e4b65eddb268214c5a
Aditya Pramana Putra <adityapramid25@gmail.com>
2026-04-17 09:12:59 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
+import { FloatingWhatsApp } from '@/components/FloatingWhatsApp';
 
 const inter = Inter({
   subsets: ['latin'],
