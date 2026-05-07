694b83c4850e71ebf90a2640c1519ff9de8cbd99
Aditya Pramana Putra <adityapramid25@gmail.com>
2026-05-07 07:52:41 +0700

    Update page.tsx

--- app/product/web-development/website-portofolio/page.tsx
+++ app/product/web-development/website-portofolio/page.tsx
@@ -8,7 +8,7 @@ import { motion } from 'motion/react';
 export default function ProductPage() {
   const product = {
     title: 'Website Portofolio',
-    price: 'Rp. 900.000',
+    price: 'Start from Rp. 1.00.000',
     category: 'Web Development',
     description: 'Ini adalah galeri digital bagi profesional, seniman, atau freelancer.',
     features: [
